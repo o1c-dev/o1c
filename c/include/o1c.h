019a34eaa5ae36a977386cdbfde10954fb246565
Matt Sicker <mattsicker@apache.org>
2021-01-21 23:41:21 -0600

    Clean up unused stuff

--- c/include/o1c.h
+++ c/include/o1c.h
@@ -1,32 +1,5 @@
-#ifndef O1C_O1C_H
-#define O1C_O1C_H
-
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdalign.h>
-
-#ifdef __clang__
-# if 100 * __clang_major__ + __clang_minor__ > 305
-#  define O1C_UNROLL _Pragma("clang loop unroll(full)")
-# endif
-#endif
-
-#ifndef O1C_UNROLL
-# define O1C_UNROLL
-#endif
-
-#if defined _MSC_VER
-# define O1C_NOINLINE __declspec(noinline)
-#else
-
-# include <unistd.h>
-
-# define O1C_NOINLINE __attribute__((noinline))
-#endif
-
-#if !defined(__unix__) && (defined(__APPLE__) || defined(__linux__))
-#define __unix__ 1
-#endif
+#ifndef o1c_H
+#define o1c_H
 
 #include "drbg.h"
 #include "util.h"
@@ -39,4 +12,4 @@
 #include "ed25519.h"
 #include "ristretto255.h"
 
-#endif //O1C_O1C_H
+#endif //o1c_H
